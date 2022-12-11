d23542986ac64049c558c7a45fe1687668d6778e
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2022-12-11 14:00:03 +0200

    Numerous new functionalities completed

--- movie.py
+++ movie.py
@@ -1,9 +1,12 @@
-import json
 import csv
+import json
+import random
+from datetime import datetime
+import sys
 
 # Possibly use tabulate
 
-
+# The UI and choices
 def main():
     global saleja
     saleja = dict()
@@ -11,41 +14,72 @@ def main():
     action = int(input("Reservation: 1\nModerator: 2\nChoice: "))
 
     if action == 1:
-        display_movies()
+        load_movies()
         movie = input("Choose a movie: ")
         reservation(movie)
+        write_data()
+
+    if action == 2:
+        try:
+            choice = input(
+                "1. Add cinema | 2. Add a new movie\n3. See reservations | 4. Quit\nNumber: "
+            )
+            if choice == "1":
+                add_cinema()
+            elif choice == "2":
+                add_movie
+            elif choice == "3":
+                ...
+            elif choice == "4":
+                write_data()
+                sys.exit("Thank you for using!")
+        except KeyboardInterrupt:
+            write_data()
+            sys.exit("Thank you!")
 
 
+        write_data()
+
+
+# Loading the data to memory
 def load_data():
     global saleja
     saleja.clear()
     with open("database.json", "r") as db:
-        dat = json.load(db)
-        # print(dat)
-        for sali in dat:
-            saleja[sali["name"]] = [
-                sali["seat_count"],
-                sali["movies"],
-            ]
-        #print(saleja)
-        print(saleja["g33ood"])
-
-        """reader = csv.reader(db)
-        next(reader)
-        #saleja[fields['name']] = [fields["available_seats"], fields["movie_name"].lower()]
-        for row in reader:
-            saleja[row[0]] = [int(row[1]), int(row[2]), row[3]]
-        print(saleja)"""
+        saleja = json.load(db)
 
 
-def add_cinema(nimi, koko):
-    ...
+# Adding a cinema
+def add_cinema():
+    global saleja
+    cinema_name = input("The name of the new cinema: ")
+    seat_count = int(input("The seat count: "))
+
+    saleja[cinema_name] = {
+        "seat_count": seat_count,
+        "movies": {time: data for (time, data) in populate(seat_count)},
+    }
 
 
+# Ading movies to the cinemas manually
 def add_movie():
-    ...
+    global saleja
+    for sali in saleja:
+        print(sali)
+
+    new = input("Which cinema: ")
+    movie = input("Which movie: ")
+    # Check if new movie in catalog
+    load_movies(movie)
+    time = input("At what time: ")
+
+    max_seats = saleja[new]["seat_count"]
+    saleja[new]["movies"][time] = {"movie_name": movie, "available_seats": max_seats}
+
+    saleja[new]["movies"] = dict(sorted(saleja[new]["movies"].items()))
 
 
+# Rerserving a seat in a selected show
 def reservation(movie: str):
     global saleja
 
@@ -53,23 +87,25 @@ def reservation(movie: str):
     # Search where the chosen movie is playing
     print("Cinema: Time: Available seats")
     for sali in saleja:
-        cin = saleja[sali]
-        name = list(cin[1].values())
 
-        for i in range(len(name)):
-            if movie.lower() == name[i]["movie_name"].lower():
-                time = list(cin[1].keys())[i]
-                print(f"{sali}: {time}: {name[i]['available_seats']}")
+        names = list(saleja[sali]["movies"].values())
+        keys = list(saleja[sali]["movies"].keys())
+
+        for i in range(len(names)):
+            if movie.lower() == names[i]["movie_name"].lower():
+                time = keys[i]
+                print(f"{sali}: {time}: {names[i]['available_seats']}")
                 playing[sali] = time
 
+    # Picking the cinema
     while True:
         cinema = input("Pick a cinema: ")
         seat = int(input("How many seats? "))
 
         chosen = playing[cinema]
 
-        if saleja[cinema][1][chosen]["available_seats"] >= seat:
-            saleja[cinema][1][chosen]["available_seats"] -= seat
+        if saleja[cinema]["movies"][chosen]["available_seats"] >= seat:
+            saleja[cinema]["movies"][chosen]["available_seats"] -= seat
             break
         else:
             print("Not enough seats, choose again.")
@@ -79,22 +115,55 @@ def reservation(movie: str):
 def write_data():
     global saleja
     with open("database.json", "w") as out:
-        fieldnames = ["name", "seat_count", "available_seats", "movie_name"]
-        for nimi in saleja:
-            ...
+        json.dump(saleja, out, indent=4)
+        out.write("\n")
 
 
-def display_movies():
-    with open("movie_catalog.csv", "r") as ctlg:
+# Print or return all the playing movies
+def load_movies(movie_name=None):
+    with open("movie_catalog.csv", "r+") as ctlg:
         reader = csv.reader(ctlg)
-        for movie in reader:
-            print(f"{movie[0]}: {movie[1]}")
+
+        if movie_name == None:
+            for movie in reader:
+                print(f"{movie[0]}: {movie[1]}")
+
+        else:
+            movies = [movie[0] for movie in reader][1:]
+            if movie_name not in movies:
+                ctlg.write(movie_name)
+                ctlg.write("\n")
 
 
 def update():
     global saleja
-    """Update the movies if enough time has passed.
-    Update the seats if movie ended."""
+    time_now = f"{datetime.now().hour}:{datetime.now().minute:02d}"
+    test_time = "20"
+
+    for cinema in saleja:
+        for time in saleja[cinema]["movies"]:
+            if int(time[:2]) <= int(test_time):
+                print(time[:2])
+                # Use the populate function
+
+
+# Populating the new cinemas randomly
+def populate(seat_count):
+    movies = list()
+    # Read the available movies into a list
+    with open("movie_catalog.csv", "r") as lista:
+        reader = csv.reader(lista)
+        for movie in reader:
+            movies.append(movie[0])
+
+    # Yield the values back to the dictionary comprehension
+    for i in range(5):
+        time = random.randint(0, 24)
+        movie = {
+            "movie_name": movies[random.randint(1, 4)],
+            "available_seats": seat_count,
+        }
+        yield time, movie
 
 
 if __name__ == "__main__":
--- time.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-time = f"{datetime.now().hour}:{datetime.now().minute:02d}"
-
-print(time)
